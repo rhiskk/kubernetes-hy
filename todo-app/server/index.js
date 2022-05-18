2e7a977e287979296213c93807c0be70a4650f61
rhiskk <hiski.r@gmail.com>
2022-05-18 10:03:31 +0300

    1.05

--- todo-app/server/index.js
+++ todo-app/server/index.js
@@ -4,6 +4,10 @@ const http = require('http');
 
 const server = http.createServer(app);
 
+app.get('/', (_req, res) => {
+    res.sendFile('1.05/hello.html', {root: __dirname })
+});
+
 server.listen(PORT, () => {
     console.log(`Server started in port ${PORT}`)
 })
